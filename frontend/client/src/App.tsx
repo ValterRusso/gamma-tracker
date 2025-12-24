3046e9df244a1fa5a19da27aef405e5010e45a3b
ValterRusso <russow@icloud.com>
2025-12-24 17:41:11 -0300

    Update App.tsx

--- frontend/client/src/App.tsx
+++ frontend/client/src/App.tsx
@@ -6,6 +6,7 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import VolatilitySurface from "./pages/VolatilitySurface";
+import VolatilitySkew from "./pages/VolatilitySkew";
 
 
 function Router() {
@@ -13,6 +14,7 @@ function Router() {
     <Switch>
       <Route path={"/"} component={Home} />
       <Route path={"/volatility-surface"} component={VolatilitySurface} />
+      <Route path="/volatility-skew" component={VolatilitySkew} />
       <Route path={"/404"} component={NotFound} />
       {/* Final fallback route */}
       <Route component={NotFound} />
