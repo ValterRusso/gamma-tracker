303add92d9e0312751a6357ec0cae806a874cc5d
ValterRusso <russow@icloud.com>
2026-01-09 06:34:34 -0300

    Create index.js

---
+++ backend/src/utils/index.js
@@ -0,0 +1,59 @@
+/**
+ * ============================================================================
+ * UTILS INDEX
+ * ============================================================================
+ * 
+ * Exporta todos os utilitários para fácil importação
+ * 
+ * USO:
+ * const { Logger, DateUtils, MathUtils } = require('./utils');
+ * 
+ * @author Valter Russo - Gamma Tracker Team
+ * @version 1.0
+ * ============================================================================
+ */
+
+module.exports = {
+  // ========================================
+  // LOGGING
+  // ========================================
+  Logger: require('./logger'),
+  
+  // // ========================================
+  // // DATE & TIME
+  // // ========================================
+  // DateUtils: require('./dateUtils'),
+  // TimeUtils: require('./timeUtils'),
+  
+  // // ========================================
+  // // MATH & STATISTICS
+  // // ========================================
+  // MathUtils: require('./mathUtils'),
+  // StatisticsUtils: require('./statisticsUtils'),
+  
+  // // ========================================
+  // // DATA PROCESSING
+  // // ========================================
+  // DataValidator: require('./dataValidator'),
+  // DataSanitizer: require('./dataSanitizer'),
+  
+  // // ========================================
+  // // FORMATTING
+  // // ========================================
+  // NumberFormatter: require('./numberFormatter'),
+  // CurrencyFormatter: require('./currencyFormatter'),
+  
+  // // ========================================
+  // // ERROR HANDLING
+  // // ========================================
+  // ErrorHandler: require('./errorHandler'),
+  // ErrorLogger: require('./errorLogger'),
+  
+  // // ========================================
+  // // PERFORMANCE
+  // // ========================================
+  // PerformanceMonitor: require('./performanceMonitor'),
+  // MemoryTracker: require('./memoryTracker'),
+  
+  // // Adicione outros utils aqui
+};
